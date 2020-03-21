694185e6f8f220f9968286234c238435556e01e4
haadi <hadiyoville25@gmail.com>
2020-03-21 10:23:21 -0700

    minor changes

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from scapy.all import *
 import argparse
 import os
 
+
 #sets up a single command line argument to be received which the
 #duration of capture (time bin)
 parser = argparse.ArgumentParser(description="Network Analysis")
-parser.add_argument("-d", type=int, dest="duration", default=1)
+parser.add_argument("-d", type=int, dest="duration", default=20)
 args = parser.parse_args()
 
-print(f"Trying to run tcpdump for {args.duration} seconds")
+#print(f"Trying to run tcpdump for {args.duration} seconds")
 
 #there is no interface supplied which means we are capturing on
 #all interfaces.
@@ -19,10 +20,14 @@ ret = os.system(f"sudo timeout {args.duration} tcpdump -w my.pcap")
 #for windos things are different. So, if you are to run it on windows,
 #change this.
 
-if ret == -1:
-    print("There was an error running the tcpdump command")
-else:
-    data = rdpcap("my.pcap")
-    print("Number of packets:", len(data))
+def test():
+    if ret == -1:
+        print("There was an error running the tcpdump command")
+    else:
+        data = rdpcap("my.pcap")
+        for data in data:
+            if data.haslayer(TCP):
+                print(data.show())
 
 
+test()
