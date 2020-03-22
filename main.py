a4ce0e9efba29994e3af1c4c16c90cd1f68e9202
haadi <hadiyoville25@gmail.com>
2020-03-22 04:49:52 -0700

    improved structure and added Packet class

--- main.py
+++ main.py
@@ -2,32 +2,46 @@ from scapy.all import *
 import argparse
 import os
 
+def SystemCmd():
+    #sets up a single command line argument to be received which the
+    #duration of capture (time bin)
+    parser = argparse.ArgumentParser(description="Network Analysis")
+    parser.add_argument("-d", type=int, dest="duration", default=20)
+    args = parser.parse_args()
 
-#sets up a single command line argument to be received which the
-#duration of capture (time bin)
-parser = argparse.ArgumentParser(description="Network Analysis")
-parser.add_argument("-d", type=int, dest="duration", default=20)
-args = parser.parse_args()
+    print(f"Trying to run tcpdump for {args.duration} seconds")
 
-#print(f"Trying to run tcpdump for {args.duration} seconds")
-
-#there is no interface supplied which means we are capturing on
-#all interfaces.
-ret = os.system(f"sudo timeout {args.duration} tcpdump -w my.pcap")
+    #there is no interface supplied which means we are capturing on
+    #all interfaces.
+    ret = os.system(f"sudo timeout {args.duration} tcpdump -w my.pcap")
 
 
-#linux returns -1 on error and pid of terminated process on success.
-#for windos things are different. So, if you are to run it on windows,
-#change this.
+
+class packet:
+    def __init__(self, s, d, p):
+        self.src = s
+        self.dst = d
+        self.prt = p
+    def print (self):
+        print ("Source: ",self.src)
+        print ("Destination: ",self.dst)
+        print ("Protocol: ",self.prt)
+
+def extract(protocol,data):
+    lst = []
+    print(protocol)   
+    for pkt in data:
+        if pkt.haslayer(protocol) == 1:
+            lst.append(packet(pkt[protocol].fields['src'],pkt[protocol].fields['dst'],protocol))
+    for obj in lst:
+        obj.print()
+
+
 
 def test():
-    if ret == -1:
-        print("There was an error running the tcpdump command")
-    else:
-        data = rdpcap("my.pcap")
-        for data in data:
-            if data.haslayer(TCP):
-                print(data.show())
+    SystemCmd()
+    data = rdpcap("my.pcap")
+    extract(IP,data)
 
 
 test()
